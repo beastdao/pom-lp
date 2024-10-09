cd56d9bdb98f0047959826230a5df078a6b09948
Amanda Vieira <amanda.alexandre@outlook.com>
2024-10-09 09:25:14 -0300

    feat: add benefit section

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Button } from 'flowbite-react'
+import data from '../benefits.json'
 
 function Benefits() {
   return (
-    <div>Benefits</div>
+    <div>
+      <h1>Benefits of Soulbound Tokens</h1>
+
+      <h2>FOR COMMUNITIES</h2>
+
+      {data.benefits.map(
+        (benefit, index) => (
+          <div key={index}>
+            <h3>{benefit.title}</h3>
+            <p>{benefit.text}</p>
+          </div>
+      )
+      )}
+
+      <Button href="https://app.pom.cards/" color='dark'>TRY NOW</Button>
+      
+    </div>
   )
 }
 
