de3378e83bb8f7545d62ecdcd022a502b92a5685
amandaalexandre <amandaalexandre@outlook.com>
2024-12-26 22:28:30 -0300

    style: fix community car sizing

--- src/components/CardImg.jsx
+++ src/components/CardImg.jsx
@@ -4,13 +4,13 @@ function CardImg({img, title, text}) {
   return (
     <div>
         <div 
-        className='bg-light-purple w-full rounded-t p-8 flex flex-row min-h-96 min-w-96 justify-center items-center'
+        className='bg-light-purple w-96 rounded-t p-8 flex flex-row h-96 w-auto justify-center items-center'
     >
-        <img src={img}/>
+        <img src={img} className='m-8 p-8 object-cover'/>
 
     </div>
     <div
-            className='bg-purple w-full rounded p-8 min-h-48'
+            className='bg-purple w-96 rounded p-8 h-52'
         >
             <h3 className='mb-4'>{title}</h3>
             <p>{text}</p>   
